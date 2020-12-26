9d0dc3f0cc6a18a2c5e34b28e2035e770fba6764
YahyaEfeOz <efeoz0816@gmail.com>
2020-12-26 20:18:30 +0300

    faktoriyel hesaplama eklendi

---
+++ faktoriyel.py
@@ -0,0 +1,11 @@
+sayı = int(input("Faktariyeli Alınacak Sayıyı Giriniz:"))
+deger = 1
+for n in range (sayı):
+        deger *= n+1
+
+if sayı <= 0:
+        print("1")
+elif sayı > 170:
+        print ("∞")
+else:
+        print(deger)
